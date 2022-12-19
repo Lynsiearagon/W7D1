e7043c6292db615808ac75e5888a34a63f7cc87e
“Shawna <shawna.e.hartley@gmail.com>
2022-12-19 15:35:19 -0500

    worked through find my creds

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -12,5 +12,36 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true 
     validates :password_digest, :session_token, presence: true 
-    validates :password, length: { minimum: 4 }, allow_nil: true 
+    validates :password, length: { minimum: 4 }, allow_nil: true
+    validates :session_token, uniqueness: true
+
+    before_validation :ensure_session_token
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+    def password=(new_pw)
+        self.password_digest = BCrypt::Password.create(new_pw)
+        @password = new_pw
+    end
+
+    def is_password?(new_pw)
+        pw_object = BCrypt::Password.new(self.password_digest)
+        pw_object.is_password?(new_pw)
+    end
+
+    def self.find_my_creds(username, password)
+        user = User.find_by(username: :username)
+
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
+    end
+
+    private
+    attr_reader :password
+
 end
